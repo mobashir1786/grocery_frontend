1394ced4dc07c02a98375b10c8a0fe602b77b533
Md Mobashir <104211044+mobashir1786@users.noreply.github.com>
2023-04-25 14:43:17 +0530

    Update Logout.jsx

--- src/componet/Logout.jsx
+++ src/componet/Logout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logout = () => {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:01 GMT";
-    document.location.href = "http://localhost:3000/loginsignup"
+    document.location.href = "https://grocery-shop-mobashir.netlify.app//loginsignup"
     return (
         <div>
             
