7b7b4158197d331d9db443628db41be9cc89c6e5
Md Mobashir <104211044+mobashir1786@users.noreply.github.com>
2023-04-27 09:27:48 +0530

    Update card.jsx

--- src/componet/card.jsx
+++ src/componet/card.jsx
@@ -22,7 +22,7 @@ export default function Card(props) {
         console.log(prod_id);
         console.log(cookie.length);
         if(cookie.length === 1){
-            window.location.href = "http://localhost:3000/loginsignup";
+            window.location.href = "https://grocery-shop-mobashir.netlify.app/loginsignup";
             return;
         }
         const token = cookie[1];
